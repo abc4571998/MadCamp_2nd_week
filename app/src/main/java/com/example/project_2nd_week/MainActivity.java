c9323a66cbd9b516f28b22aec5a67d317f8e44ed
yunseokjeong <wjddbstjr03@naver.com>
2022-07-07 23:12:17 +0900

    start

---
+++ app/src/main/java/com/example/project_2nd_week/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.project_2nd_week;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
