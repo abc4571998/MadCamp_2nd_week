e12d0fee254f69973922ef220fc5e330139b577d
yunseokjeong <wjddbstjr03@naver.com>
2022-07-08 02:57:37 +0900

    start

--- app/src/main/java/com/example/project_2nd_week/KakaoLogin/LoginActicity.java
+++ app/src/main/java/com/example/project_2nd_week/KakaoLogin/LoginActicity.java
@@ -1,8 +1,11 @@
 package com.example.project_2nd_week.KakaoLogin;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import com.example.project_2nd_week.R;
 import com.example.project_2nd_week.databinding.ActicityLoginBinding;
@@ -10,12 +13,19 @@ import com.example.project_2nd_week.databinding.ActicityLoginBinding;
 public class LoginActicity extends AppCompatActivity {
 
     private ActicityLoginBinding binding;
+    private KakaoLoginLogoutManager manager = new KakaoLoginLogoutManager(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.acticity_login);
+        binding = DataBindingUtil.setContentView(this, R.layout.acticity_login);
 
+        binding.loginButton.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                manager.signInKakao();
+            }
+        });
 
     }
 
